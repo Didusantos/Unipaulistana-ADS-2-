9e4ba34d1b87f898ee75e4ec8f71b3f6512e8858
Didusantos <diduporto@hotmail.com>
2025-09-03 16:27:45 -0300

    teste para subir no git

--- src/main/java/com/example/demo/A.java
+++ src/main/java/com/example/demo/A.java
@@ -1,4 +1,12 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class A {
+    @GetMapping(path = "/teste")
+    public String teste(){
+        return "Isso é um teste";
+    }
 }
---
+++ src/test/java/com/example2222/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example2222.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
